8a35b009454d5f60048f9a4d64ca6fc2e3b14da8
Joseph Ojuko <josephojuko2020@gmail.com>
2023-02-24 02:44:56 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples of two integers
+ * @a: nParameter one
+ * @b: second num
+ * Return: multiplicationm
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
