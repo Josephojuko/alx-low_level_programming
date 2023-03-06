d6c9d024df041bf573c86b65e1909a752f0a556d
Joseph Ojuko <josephojuko2020@gmail.com>
2023-03-06 04:51:07 -0800

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of string
+ * @s: pointer to pointer
+ * @to: pointer character
+ * Return: always 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
