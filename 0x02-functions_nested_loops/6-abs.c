538a274f932fbb31f44e5bcc6a88f61bee95c5b2
Josephojuko <josephojuko2020.com>
2023-05-23 09:22:32 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an inte
+* @n: the int to chec
+* Return: the absolute value of int
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
