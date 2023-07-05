6f7dd0b1867a50ce851382393ae444d34f1ebc7a
Josephojuko <josephojuko2020.com>
2023-07-05 09:28:03 -0700

    task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,17 +2,17 @@
 /**
 * add_nodeint - adds a new node st 
 * @head: pointer to list
-* @i: data tonode
+* @n: data tonode
 * Return: pointer tNULL if it fails
 */
-listint_t *add_nodeint(listint_t **head, const int i)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	new->i = i;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 	return (new);
